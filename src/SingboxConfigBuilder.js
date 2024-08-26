0d9d4883e4aef2356db9776ee1c3eb76b0115650
7Sageer <125936732+7Sageer@users.noreply.github.com>
2024-08-26 16:54:38 +0800

    fix: wrong console

--- src/SingboxConfigBuilder.js
+++ src/SingboxConfigBuilder.js
@@ -74,8 +74,6 @@ export class ConfigBuilder extends BaseConfigBuilder {
 
         this.config.route.rule_set = [...site_rule_sets, ...ip_rule_sets];
 
-        console.log(rules[7].ip_rules);
-
         this.config.route.rules = rules.map(rule => ({
             rule_set: rule.site_rules.length > 0 && rule.site_rules[0] !== '' ? 
                       [...rule.site_rules, ...rule.ip_rules.filter(ip => ip.trim() !== '').map(ip => `${ip}-ip`)] : 
