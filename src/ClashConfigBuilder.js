773bc3d8dbf0326b3dce3a6c3e0f256b71772680
Kimi_TT <69085599+fantasy-07@users.noreply.github.com>
2025-02-12 20:30:29 +0800

    Update ClashConfigBuilder.js

--- src/ClashConfigBuilder.js
+++ src/ClashConfigBuilder.js
@@ -118,9 +118,9 @@ export class ClashConfigBuilder extends BaseConfigBuilder {
 		    //2025.2.12修改：alterId 默认为 0
 		    alterId: 0,
                     cipher: proxy.security,
-                    tls: proxy.tls?.enabled || false,
+                    //tls: proxy.tls?.enabled || false,
 		    //2025.2.12修改：proxy.tls 的值为 tls，则返回 true，否则返回 false
-		    //tls: proxy.tls === 'tls' || false,
+		    tls: proxy.tls,
                     servername: proxy.tls?.server_name || '',
                     network: proxy.transport?.type || 'tcp',
                     'ws-opts': proxy.transport?.type === 'ws' ? {
