66fb8709051c6e7c23f810ea6fb6f43865d8a4e8
ziyosen <lozystore@outlook.co.id>
2025-07-27 04:01:33 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -383,7 +383,7 @@ export function generateClashRuleSets(selectedRules = [], customRules = []) {
 
 // Singbox configuration
 export const SING_BOX_CONFIG = {
-{
+
     "log": {
         "disabled": true,
         "level": "debug",
