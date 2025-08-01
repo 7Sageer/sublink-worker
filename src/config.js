394bd7aa520234c3892b12c466d299df618d69e1
ziyosen <lozystore@outlook.co.id>
2025-08-02 05:55:14 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 import { t } from './i18n';
 
-
+export const SITE_RULE_SET_BASE_URL = 'https://gh-proxy.com/https://github.com/NSZA156/surge-geox-rules/raw/refs/heads/release/geo/geoip/';
+export const IP_RULE_SET_BASE_URL = 'https://gh-proxy.com/https://github.com/NSZA156/surge-geox-rules/raw/refs/heads/release/geo/geosite/';
 export const CLASH_SITE_RULE_SET_BASE_URL = 'https://gh-proxy.com/https://github.com/MetaCubeX/meta-rules-dat/raw/refs/heads/meta/geo/geosite/';
 export const CLASH_IP_RULE_SET_BASE_URL = 'https://gh-proxy.com/https://github.com/MetaCubeX/meta-rules-dat/raw/refs/heads/meta/geo/geoip/';
 export const SURGE_SITE_RULE_SET_BASEURL = 'https://gh-proxy.com/https://github.com/NSZA156/surge-geox-rules/raw/refs/heads/release/geo/geosite/'
