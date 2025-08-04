93e165bb865f8787ca75b28facd7695c7ae41a54
ziyosen <lozystore@outlook.co.id>
2025-08-05 06:54:41 +0700

    Update htmlBuilder.js

--- src/htmlBuilder.js
+++ src/htmlBuilder.js
@@ -58,14 +58,14 @@ const generateDarkModeToggle = () => `
 `;
 
 const generateGithubLink = () => `
-  <a href="https://github.com/7Sageer/sublink-worker" target="_blank" rel="noopener noreferrer" class="github-link">
-    <i class="fab fa-github"></i>
+  <a href="https://t.me/Bleszh" target="_blank" rel="noopener noreferrer" class="telegram-link">
+    <i class="fab fa-telegram"></i>
   </a>
 `;
 
 const generateCardHeader = () => `
   <div class="card-header text-center">
-    <h1 class="display-4 mb-0">Sublink Worker</h1>
+    <h1 class="display-4 mb-0">Generate Link</h1>
   </div>
 `;
 
