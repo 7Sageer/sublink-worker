11620624c449bb00eee9c17b149cfc2f0944ee64
ziyosen <lozystore@outlook.co.id>
2025-08-08 05:07:30 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import { t } from './i18n';
-export const SITE_RULE_SET_BASE_URL = 'https://raw.githubusercontent.com/SagerNet/sing-geosite/rule-set/geosite-cn.srs';
-export const IP_RULE_SET_BASE_URL = 'https://raw.githubusercontent.com/SagerNet/sing-geoip/rule-set/geoip-cn.srs';
+export const SITE_RULE_SET_BASE_URL = 'https://testingcf.jsdelivr.net/gh/lyc8503/sing-box-rules@rule-set-geosite/geosite-cn.srs';
+export const IP_RULE_SET_BASE_URL = 'https://testingcf.jsdelivr.net/gh/lyc8503/sing-box-rules@rule-set-geoip/geoip-cn.srs';
 export const CLASH_SITE_RULE_SET_BASE_URL = 'https://gh-proxy.com/https://github.com/MetaCubeX/meta-rules-dat/raw/refs/heads/meta/geo/geosite/';
 export const CLASH_IP_RULE_SET_BASE_URL = 'https://gh-proxy.com/https://github.com/MetaCubeX/meta-rules-dat/raw/refs/heads/meta/geo/geoip/';
 export const SURGE_SITE_RULE_SET_BASEURL = 'https://gh-proxy.com/https://github.com/NSZA156/surge-geox-rules/raw/refs/heads/release/geo/geosite/'
