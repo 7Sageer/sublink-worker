fe8e3d1b5a90744782e384fa841422b0505cfdb4
7Sageer <12210216@mail.sustech.edu.cn>
2025-12-26 13:04:50 +0800

    chore: release v2.2.0

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 // Application constants
 export const APP_NAME = 'Sublink Worker';
-export const APP_VERSION = '2.1.1';
+export const APP_VERSION = '2.2.0';
 export const GITHUB_REPO = 'https://github.com/7Sageer/sublink-worker';
 export const GITHUB_API_RELEASES = 'https://api.github.com/repos/7Sageer/sublink-worker/releases/latest';
 export const DOCS_URL = 'https://sublink.works';
